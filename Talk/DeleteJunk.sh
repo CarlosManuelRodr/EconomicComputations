f12dcd1169a94042c1ff0914eddf750147db149c
Carlos Manuel <fis.carlosmanuel@gmail.com>
2019-01-31 13:23:33 -0600

    Applications

---
+++ Talk/DeleteJunk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+rm -rf *eps-converted-to.pdf
+rm -f *.synctex.gz
+rm -f *.aux
+rm -f *.log
+rm -f *.fdb_latexmk
+rm -f *.spl
+rm -f *.fls
+rm -f *.blg
+rm -f *.out
+rm -f *.nav
+rm -f *.dvi
+rm -f *.bcf
+rm -f *.xml
+rm -f *.snm
+rm -f *.toc
+rm -f *.bak
+rm -f *.bbl
